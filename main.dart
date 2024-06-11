eb7688766f90520c52ff8bd5b002dfae2dbd7bd3
Silas325 <hakusilas@gmail.com>
2024-06-11 10:35:57 +0200

    committed 3

--- main.dart
+++ main.dart
@@ -1,5 +1,13 @@
-void main(){
-String name="Silas";
-print("Good morning" +name);
+/// This is the main entry point of the application.
+/// It prints a greeting message with the name "Silas" and the current date and tim,
+
+void main() {
+  String name = "Silas";
+  String Profile="Administrator";
+  print("Good morning" + name);
+  print(DateTime.now());
+
+  //welcome
+  print("welcome" +Profile);
 }
 
