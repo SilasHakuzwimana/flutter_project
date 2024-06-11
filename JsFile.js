9f4b3214585c766afbc53b76b9beefae08a8401c
Silas325 <hakusilas@gmail.com>
2024-06-11 12:43:21 +0200

    committed

--- JsFile.js
+++ JsFile.js
@@ -1,2 +1,4 @@
-var name='Silas';
-document.write("Hello"+name);
+let Username='Silas';
+document.write("Hello"+ Username);
+const x =35+34*4-5/2;
+document.write(x);
--- main.dart
+++ main.dart
@@ -1,6 +1,7 @@
 /// This is the main entry point of the application.
 /// It prints a greeting message with the name "Silas" and the current date and tim,
 
+import 'dart:math';
 void main() {
   String name = "Silas";
   String Profile="Administrator";
@@ -8,6 +9,15 @@ void main() {
   print(DateTime.now());
 
   //welcome
-  print("welcome" +Profile);
+  print("welcome" +" "+Profile);
+int a=3;
+int b=20;
+int c=a*b;
+print("The result is:");
+print(c);
+
+int p=a*a;
+print(p);
+
 }
 
